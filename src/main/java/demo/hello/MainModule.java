5b5196b9a619ddceb1ac480af9023e5f9967036a
mzywtj <1192016076@qq.com>
2017-09-24 20:17:57 +0800

    注释

--- src/main/java/demo/hello/MainModule.java
+++ src/main/java/demo/hello/MainModule.java
@@ -7,8 +7,8 @@ import org.nutz.mvc.annotation.Ok;
  * Created by mzy on 17/9/24.
  */
 public class MainModule {
-    @At("/hello")
-    @Ok("jsp:jsp.hello")
+    @At("/hello")//请求路径
+    @Ok("jsp:jsp.hello")//返回页面地址
     public String doHello() {
         return "Hello Nutz";
     }
